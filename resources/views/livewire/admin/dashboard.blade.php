9ff454237a23900e9b39f343bad4cf7aaac2a6c2
Enrique <enrique98isc@gmail.com>
2026-06-04 13:29:38 -0600

    aupdate daschborad admin

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -34,17 +34,6 @@
                 <p class="text-4xl font-mono font-black italic">{{ number_format($totalUsuarios) }}</p>
             </div>
 
-            <!-- Tickets Pendientes 
-            <div class="bg-gray-800 border border-gray-700 p-6 rounded-2xl shadow-xl">
-                <div class="flex justify-between items-start mb-4">
-                    <div class="p-3 bg-yellow-600/10 rounded-xl">
-                        <i class="fas fa-clock text-yellow-600"></i>
-                    </div>
-                </div>
-                <h3 class="text-gray-500 text-xs font-black uppercase tracking-tighter">Puntajes Por Validar</h3>
-                <p class="text-4xl font-mono font-black italic text-yellow-500">{{ $puntajePendiente }}</p>
-            </div>-->
-
             <!-- Intentos Realizados -->
             <div class="bg-gray-800 border border-gray-700 p-6 rounded-2xl shadow-xl">
                 <div class="flex justify-between items-start mb-4">
